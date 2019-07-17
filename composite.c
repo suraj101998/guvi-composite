ea5652d44c3521ad0151c50c40050d2cc8c944eb
suraj101998 <51699223+suraj101998@users.noreply.github.com>
2019-07-17 09:38:32 +0530

    Add files via upload

---
+++ composite.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,factor;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+         if(n%i==0) 
+         {
+             factor=i;
+         }
+    }
+    if(factor>1) 
+    {
+          printf ("yes");
+    }
+    else 
+    {
+          printf ("no");
+    }
+    return 0;
+}
